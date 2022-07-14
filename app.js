62cea5745c67ea02f56d57a08bea21ba4d29d936
ohampton <44102431+ohampton@users.noreply.github.com>
2022-07-14 14:54:50 -0500

    Render Movie Properties

--- app.js
+++ app.js
@@ -76,5 +76,54 @@ const onMovieSelect = async (movie) => {
         }
     });
 
-    console.log(response.data);
+    document.querySelector('#summary').innerHTML = movieTemplate(response.data)
+};
+
+const movieTemplate = (movieDetail) => {
+    return `
+    <article class="media">
+    <figure class="media-left">
+        <p class="image">
+            <img src="${movieDetail.Poster}"/>
+        </p>
+    </figure>
+  
+    <div class="media-content">
+        <div class="content">
+            <h1>${movieDetail.Title}</h1>
+            <h4>${movieDetail.Genre}</h4>
+            <p>${movieDetail.Plot}</p>
+        </div>
+    </div>
+  </article>
+  <article>
+  <article class="notification is-primary">
+    <p class="title">${movieDetail.Awards}</p>
+    <p class="sub-title">Awards</p>
+</article>
+<article class="notification is-primary">
+    <p class="title">${movieDetail.BoxOffice}</p>
+    <p class="sub-title">Box Office</p>
+</article>
+<article class="notification is-primary">
+    <p class="title">${movieDetail.Metascore}</p>
+    <p class="sub-title">Metascore</p>
+</article>
+<article class="notification is-primary">
+    <p class="title">${movieDetail.imdbRating}</p>
+    <p class="sub-title">IMDB Rating</p>
+</article>
+<article class="notification is-primary">
+    <p class="title">${movieDetail.imdbVotes}</p>
+    <p class="sub-title">IMDB Votes</p>
+</article>  
+</article>
+    
+    
+    
+    
+    
+    
+    
+    `
 };

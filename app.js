4773b2f08f764c947526e4fcf6328fd51431c72e
ohampton <44102431+ohampton@users.noreply.github.com>
2022-07-14 14:28:17 -0500

    get movie request pre rendering

--- app.js
+++ app.js
@@ -33,6 +33,11 @@ const resultsWrapper = document.querySelector('.results');
 const onInput = async event => {
     const movies = await fetchData(event.target.value)
 
+    if (!movies.length) {
+        dropdown.classList.remove('is-active');
+        return
+    }
+
     resultsWrapper.innerHTML = '';
     dropdown.classList.add('is-active');
     for (let movie of movies) {
@@ -43,7 +48,13 @@ const onInput = async event => {
         option.innerHTML = `
         <img src="${imgSrc}"/>
         ${movie.Title}
-        `
+        `;
+
+        option.addEventListener('click', () => {
+            dropdown.classList.remove('is-active')
+            input.value = movie.Title;
+            onMovieSelect(movie);
+        });
 
         resultsWrapper.appendChild(option);
     }
@@ -55,3 +66,15 @@ document.addEventListener('click', event => {
         dropdown.classList.remove('is-active');
     }
 });
+
+const onMovieSelect = async (movie) => {
+    const response = await axios.get('http://www.omdbapi.com/',
+    {
+        params: {
+            apikey: 'c964f021',
+            i: movie.imdbID
+        }
+    });
+
+    console.log(response.data);
+};
